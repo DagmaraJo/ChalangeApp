b6836ffc587609504f7d3a13009c7d32d61be87e
DagmaraJo <djodko@gmail.com>
2024-02-19 14:07:10 +0100

    dzień 4 - Ewa - if-if else

--- ChalangeApp/Program.cs
+++ ChalangeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var age = 30;
+string sex = "man";
+
+if(age < 30 && sex =! "man")
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if( age == 30 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if(age < 18 && sex == "man")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
