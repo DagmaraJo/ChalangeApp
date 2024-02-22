ccbaaedbb1093bfc4df4134727556dd21fef8522
DagmaraJo <djodko@gmail.com>
2024-02-22 05:24:51 +0100

    krok 5

--- ChalangeApp/Program.cs
+++ ChalangeApp/Program.cs
@@ -1,16 +1,17 @@
-﻿string name = "Ewa";
-var age = 30;
-string sex = "man";
+﻿int number = 4566;
+string numText = number.ToString();
+char[] letters = numText.ToArray();
+Console.WriteLine("Wyniki dla licby:  " + numText);
 
-if(age < 30 && sex =! "man")
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if( age == 30 && name == "Ewa")
-{
-    Console.WriteLine("Ewa, lat 30");
-}
-else if(age < 18 && sex == "man")
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+//string[] setOfNumbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+int[] digit = new int[10];
+
+foreach (char letter in numText)
+    {
+        int letterValue = (int)Char.GetNumericValue(letter);
+        digit[letterValue]++;
+
+    }
+for (var i = 0; i < 10; i++)
+
+    Console.WriteLine( i + " =>  " + digit[i]);
