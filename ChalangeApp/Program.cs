75ca0945b5ada745dd2fe53bbbde5311893ece4d
DagmaraJo <djodko@gmail.com>
2024-02-23 19:49:15 +0100

    dzień 5 - pętle for/ foreach w listach i tablicach

--- ChalangeApp/Program.cs
+++ ChalangeApp/Program.cs
@@ -2,27 +2,12 @@
 string numText = number.ToString();
 Console.WriteLine("Wyniki dla licby:  " + numText);
 
+List<char> leters = new() { '0', '1', '3', '4', '5', '6', '7', '8', '9' };
 int[] digit = new int[10];
 
 foreach (char letter in numText)
 {
-    int letterValue = (int)Char.GetNumericValue(letter);
-    digit[letterValue]++;
-
+    for (var i = 0; i < numText.Length; i++);
 }
 for (var i = 0; i < 10; i++)
-
     Console.WriteLine(i + " =>  " + digit[i]);
-
-
-
-
-
-
-
-
-
-
-
-
-
