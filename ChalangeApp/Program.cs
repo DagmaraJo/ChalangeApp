a1659a3f0825f8a46a084b66cde39be525729c91
DagmaraJo <djodko@gmail.com>
2024-03-02 23:18:22 +0100

    6 poprawka wiek pracownika do pub wiad

--- ChalangeApp/Program.cs
+++ ChalangeApp/Program.cs
@@ -1,29 +1,33 @@
 ﻿using ChalangeApp;
-Employee emp1 = new ("Jan", "Kowalski", "39");
-Employee emp2 = new ("Ewa", "Lorek","28");
-Employee emp3 = new ("Piotr", "Banach", "34");
+
+Employee emp1 = new Employee ("Jan", "Kowalski", "39");
+Employee emp2 = new Employee ("Ewa", "Lorek","28");
+Employee emp3 = new Employee ("Piotr", "Banach", "34");
 List<Employee> employees = new List<Employee>()
 {
     emp1, emp2, emp3
 };
+
 emp1.AddGrade(3);
 emp1.AddGrade(2);
 emp1.AddGrade(4);
 emp1.AddGrade(3);
 emp1.AddGrade(6);
+
 emp2.AddGrade(2);
 emp2.AddGrade(4);
 emp2.AddGrade(5);
 emp2.AddGrade(6);
 emp2.AddGrade(7);
+
 emp3.AddGrade(0);
 emp3.AddGrade(0);
 emp3.AddGrade(1);
 emp3.AddGrade(2);
 emp3.AddGrade(2);
-Employee bestEmp = null ;
-int bestResult = -1;
 
+Employee bestEmp = null;
+int bestResult = -1;
 foreach ( var emp in employees)
 {
     if (emp.Result >= bestResult)
@@ -34,5 +38,6 @@ foreach ( var emp in employees)
 }
 if (bestEmp != null)
 {
-    Console.WriteLine($"Employee of the week : {bestEmp.Name} {bestEmp.Surname}   BEST RESULT : {bestResult} scores");
+    Console.WriteLine($"Employee of the week : {bestEmp.Name} {bestEmp.Surname} ({bestEmp.Age})");
+    Console.WriteLine($"BEST_RESULT_:_{bestResult}_scores");
 }
