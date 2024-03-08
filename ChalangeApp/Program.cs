975623405dcad2acd437d60d08a6ba217995768e
DagmaraJo <djodko@gmail.com>
2024-03-08 08:09:31 +0100

    zad 11    bez zbędnego numerowania statistics w 4 schematach  pętli

--- ChalangeApp/Program.cs
+++ ChalangeApp/Program.cs
@@ -8,7 +8,6 @@ employee5.AddGrade(35.5555555555559);
 employee5.AddGrade(374920700936);
 employee5.AddGrade('d');
 
-var statistics = employee5.GetStatistics();
 var statistics1 = employee5.GetStatisticsWithForeach();
 var statistics2 = employee5.GetStatisticsWithFor();
 var statistics3 = employee5.GetStatisticsWithDoWhile();
@@ -18,7 +17,7 @@ var statistics4 = employee5.GetStatisticsWithWhile();
 //Console.WriteLine($"Min:  {statistics.Min:N2}");
 //Console.WriteLine($"Max :  {statistics.Max:N2}");
 
-Console.WriteLine($"  Min_{statistics1.Min:N2}/ Average_{statistics1.Average:N2}/ Max_{statistics1.Max:N2} Foreach");
-Console.WriteLine($"  Min_{statistics2.Min:N2}/ Average_{statistics2.Average:N2}/ Max_{statistics2.Max:N2} For");
-Console.WriteLine($"  Min_{statistics3.Min:N2}/ Average_{statistics3.Average:N2}/ Max_{statistics3.Max:N2} DoWhile");
-Console.WriteLine($"  Min_{statistics4.Min:N2}/ Average_{statistics4.Average:N2}/ Max_{statistics4.Max:N2} While");
+Console.WriteLine($"  Min_{statistics1.Min:N2} Average_{statistics1.Average:N2} Max_{statistics1.Max:N2} withForeach");
+Console.WriteLine($"  Min_{statistics2.Min:N2} Average_{statistics2.Average:N2} Max_{statistics2.Max:N2} withFor");
+Console.WriteLine($"  Min_{statistics3.Min:N2} Average_{statistics3.Average:N2} Max_{statistics3.Max:N2} withDoWhile");
+Console.WriteLine($"  Min_{statistics4.Min:N2} Average_{statistics4.Average:N2} Max_{statistics4.Max:N2} withWhile");
